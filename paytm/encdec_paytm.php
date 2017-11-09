5cfe53a82c59614262433307f26fbc78f2a85e9b
aabhishekgoyal <abhishek.goyal@paytm.com>
2017-11-09 14:40:09 +0530

    Update encdec_paytm.php

--- paytm/encdec_paytm.php
+++ paytm/encdec_paytm.php
@@ -80,7 +80,7 @@ function getChecksumFromArray($arrayList, $key, $sort=1) {
 function verifychecksum_e($arrayList, $key, $checksumvalue) {
 	$arrayList = removeCheckSumParam($arrayList);
 	ksort($arrayList);
-	$str = getArray2Str($arrayList);
+	$str = getArray2StrForVerify($arrayList);
 	$paytm_hash = decrypt_e($checksumvalue, $key);
 	$salt = substr($paytm_hash, -4);
 
@@ -111,6 +111,20 @@ function getArray2Str($arrayList) {
 			continue;
 		}
 		
+		if ($flag) {
+			$paramStr .= checkString_e($value);
+			$flag = 0;
+		} else {
+			$paramStr .= "|" . checkString_e($value);
+		}
+	}
+	return $paramStr;
+}
+
+function getArray2StrForVerify($arrayList) {
+	$paramStr = "";
+	$flag = 1;
+	foreach ($arrayList as $key => $value) {
 		if ($flag) {
 			$paramStr .= checkString_e($value);
 			$flag = 0;
