a3f0b9839c326657eb421538745a9d78ea65a98b
Abhishek Awasthi <74229697+abpaytm@users.noreply.github.com>
2021-07-28 18:37:03 +0530

    Update version.php

--- moodle/paytm/version.php
+++ moodle/paytm/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020112300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2021072800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'enrol_paytm';    // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 60;
